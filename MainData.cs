61ce66bcb92fa480421787558e39333adf2a5c40
Eric Chauvin <162734842+EricsSourceCode@users.noreply.github.com>
2024-11-20 13:53:17 -0700

    Add files via upload

--- MainData.cs
+++ MainData.cs
@@ -22,7 +22,7 @@ using System.Windows.Forms;
 public class MainData
 {
 internal const string versionDate =
-                              "11/14/2024";
+                              "11/20/2024";
 private string dataDirectory = "";
 // internal Configure config;
 private MainForm mForm;
